4c2dae6b26235135a35ca82c5f49a514aa7b728c
Aragita <tiaratyagita04@gmail.com>
2023-03-12 22:19:34 +0700

    Menambahkan Exercise01

---
+++ Exercise01.py
@@ -0,0 +1,16 @@
+#input 
+Nama = 'Tiara Tyagita Azzahra'
+Nim = 'I0322122'
+Alamat = 'Totosari Pajang Laweyan Surakarta'
+Visualisasi = 'terkenal dengan sebutan Kampung Batik, memiliki akses jalan yang nyaman dan memadai, banyak kucing liar yang lucu, tetangga yang ramah dan saling mensupport'
+Harapan = 'saya ingin menjadi lulusan teknik industri  yang sukses, salah satunya  untuk mendapatkan beasiswa s2 di luar negri dan magang/bekerja di perusahaan bergengsi'
+
+dict = {'a' : Nama, 'b' : Nim, 'c' : Alamat, 'd' : Visualisasi, 'e' : Harapan}
+
+
+#hasil
+print("Nama saya", dict['a'])
+print("Nim saya adalah", dict ['b'])
+print("Saya tinggal di", dict['c'])
+print("Kampung Halaman saya", dict['d'])
+print("Harapan saya sebagai lulusan teknik industri adalah", dict ['e'])
